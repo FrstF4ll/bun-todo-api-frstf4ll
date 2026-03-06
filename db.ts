382819318a108bcfdc5b6b6ffa03dccc5ae40547
FrstF4ll <hydralixe@gmail.com>
2026-03-06 10:38:29 +0100

    revert: set back default value for done

--- db.ts
+++ db.ts
@@ -7,7 +7,7 @@ const query = db.query(`CREATE TABLE IF NOT EXISTS todos
     title VARCHAR(150) NOT NULL,
     content VARCHAR(250),   
     due_date VARCHAR(20),
-    done BOOLEAN NOT NULL
+    done BOOLEAN NOT NULL default 0
     )`);
 query.run();
 
