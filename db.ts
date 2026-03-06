d035f96a3826d1126048963794f6d5483d546243
Tiziano Dimo <hydralixe@gmail.com>
2026-03-06 09:57:53 +0100

    feat: database setup (#9)
    * chore: create database file
    
    * feat: import bun native sqlite3 database
    
    * chore: make git ignore database
    
    * feat: create database query.
    
    Create database query file.
    Run the script with 'bun run db.ts' to create the database
    closes #2
    
    * fix: correct creation query.
    
    - Set up autoincrement for id, and remove base not null.
    - Add condition of creation 'if not exists'.
    - Also export 'db' for external use and later access it for data operations

---
+++ db.ts
@@ -0,0 +1,14 @@
+import { Database } from "bun:sqlite";
+
+const db = new Database("todolist.sqlite", { create: true });
+const query = db.query(`CREATE TABLE IF NOT EXISTS todos
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(150) NOT NULL,
+    content VARCHAR(250),   
+    due_date VARCHAR(20),
+    done BOOLEAN NOT NULL
+    )`);
+query.run();
+
+export default db;
