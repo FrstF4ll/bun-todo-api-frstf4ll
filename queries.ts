2c3b580fa2c45b7844268565fd2350131cb79fb2
FrstF4ll <hydralixe@gmail.com>
2026-03-06 13:12:28 +0100

    feat: build 'post' function

--- queries.ts
+++ queries.ts
@@ -6,4 +6,13 @@ export const getTodos = () => {
     catch {
         throw new Error("Failed to get datas")
     }
-}
+};
+
+export const createTodo = () => {
+    try {
+        return db.query(`INSERT INTO todos(title, content, due_date, done)
+                             VALUES ($title, $content, $due_date, $done)`)
+    } catch {
+        throw new Error("Failed to get datas")
+    }
+};
