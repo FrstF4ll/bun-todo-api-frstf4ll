2a2db7bde608c65b171b35efcfa892cbae7429f9
FrstF4ll <hydralixe@gmail.com>
2026-03-06 08:14:47 +0100

    feat: create database query.
    Create database query file.
    Run the script with 'bun run db.ts' to create the database
    closes #2

--- db.ts
+++ db.ts
@@ -1,5 +1,12 @@
 import { Database } from "bun:sqlite";
 
-const db = new Database(":memory:");
-const query = db.query("select 'Hello world' as message;");
-query.get();
+const db = new Database("todolist.sqlite", { create: true });
+const query = db.query(`CREATE TABLE todos
+(
+    id INTEGER PRIMARY KEY NOT NULL,
+    title VARCHAR(150) NOT NULL,
+    content VARCHAR(250),   
+    due_date VARCHAR(20),
+    done BOOLEAN NOT NULL
+    )`);
+query.run();
