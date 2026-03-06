bb2a9a0d47997ffea3fd8961ebbd0cfaad301964
FrstF4ll <hydralixe@gmail.com>
2026-03-06 16:03:06 +0100

    refactor(validation): implement type for result
    - Extract schemas out of the function
    - Use type inference on output to ensure the type of the data correspond to the todo's rules.

--- valibot.ts
+++ valibot.ts
@@ -2,17 +2,25 @@ import * as v from 'valibot';
 import {string} from "valibot";
 
 
-export function validateSchema(data: any) {
-    const DoneSchema = v.pipe(v.boolean(), v.transform((val) =>
-        val ? 1 : 0))
-    const dueDateSchema = v.pipe(v.string(), v.isoDate())
-    const TodoSchema = v.strictObject({
-         $title: v.string(),
-         $content: v.string(),
-         $due_date: dueDateSchema,
-         $done: DoneSchema
-        })
-        const result = v.safeParse(TodoSchema, data)
+type ValidationResult =
+    | { success: true; data: TodoOutput }
+    | { success: false; errors: v.FlatErrors<undefined> };
+type TodoOutput = v.InferOutput<typeof TodoSchema>;
+
+const dueDateSchema = v.pipe(v.string(), v.isoDate())
+
+const DoneSchema = v.pipe(v.boolean(), v.transform((val) =>
+    val ? 1 : 0))
+
+const TodoSchema = v.strictObject({
+    $title: v.string(),
+    $content: v.string(),
+    $due_date: dueDateSchema,
+    $done: DoneSchema
+})
+
+export function validateSchema(data: any): ValidationResult {
+    const result = v.safeParse(TodoSchema, data)
     if (result.success) {
         return { success: true, data: result.output };
     } else {
