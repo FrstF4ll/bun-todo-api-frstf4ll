6cfab7a28543a68c1e83d52677312cd640cf72d7
FrstF4ll <hydralixe@gmail.com>
2026-03-06 16:10:10 +0100

    feat: fully implement post method.
    - Implement valibot inside the main function
    
    closes #4

--- index.ts
+++ index.ts
@@ -1,12 +1,28 @@
-import index from './index.html'
 import { initDB } from "./db.ts";
-import { getTodos } from './queries.ts';
+import {createTodo, getTodos} from './queries.ts';
+import {validateSchema} from "./valibot.ts";
+interface Todo {
+  $title: string;
+  $content: string | null;
+  $due_date: string | null;
+  $done: 0 | 1;
+}
+
 initDB();
 const server = Bun.serve({
   port: 3000,
   routes: {
     "/todos": {
-      GET: () => Response.json(getTodos())
+      GET: () => Response.json(getTodos()),
+      POST: async req => {
+        const body = await req.json();
+        const validation = validateSchema(body)
+        if (!validation.success) {
+          return Response.json(validation.errors, {status: 400})
+        }
+        await createTodo(validation.data)
+        return Response.json({created: true, ...validation.data})
+      }
     },
   }
 });
