8d19718bcea5c6a549996665d75d1ae51704c2cc
FrstF4ll <hydralixe@gmail.com>
2026-03-06 11:51:59 +0100

    refactor: wrap database initiation in a single function

--- db.ts
+++ db.ts
@@ -1,15 +1,14 @@
 import { Database } from "bun:sqlite";
 
 const db = new Database("todolist.sqlite", { create: true });
-const create = db.query(`CREATE TABLE IF NOT EXISTS todos
+export const initDB = () => db.query(`CREATE TABLE IF NOT EXISTS todos
 (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title VARCHAR(150) NOT NULL,
     content VARCHAR(250),   
     due_date VARCHAR(20),
     done BOOLEAN NOT NULL default 0
-    )`);
-export const initDB = () => create.run();
+    )`).run();
 export const getTodos = () => {
     try {
         return db.query(`select * from todos`).all()
